2ad96c85ff8a6f2b6496e61d8e6b067eb3ce094e
Fabiano <fabianoyoschitaki@gmail.com>
2018-12-06 00:40:39 -0200

    fib

--- src/MainDynamicProgrammingFibonacci.java
+++ src/MainDynamicProgrammingFibonacci.java
@@ -4,11 +4,11 @@ public class MainDynamicProgrammingFibonacci {
 	static int CONT;
 	
 	public static void main(String[] args) {
-		Integer n = 5;
+		Integer n = 8;
 		Integer [] fibCache = new Integer[n+1];
-		System.out.println(fibDyn(n, fibCache));
+		System.out.println("Fib for " + n + " is: " + fibDyn(n, fibCache));
 		for (int cont = 0; cont < fibCache.length; cont++) {
-			System.out.println("Fib for [" + cont + "] is: " + fibCache[cont]);
+			System.out.println("Cached Fib for [" + cont + "] is: " + fibCache[cont]);
 		}
 		System.out.println("Cache size: " + fibCache.length);
 		System.out.println("Total method calls: " + CONT);
@@ -16,16 +16,13 @@ public class MainDynamicProgrammingFibonacci {
 
 	private static Integer fibDyn(int n, Integer[] fibCache) {
 		CONT++;
-		System.out.println("fibDyn of " + n);
+		if (fibCache[n] != null)  
+			return fibCache[n];
+		
 		if (n <= 1)
-			return n;
-		if (fibCache[n] != null) { 
-			System.out.println("Cached!");
-			return fibCache[n];
-		} else {
-			System.out.println("Not Cached!");
+			fibCache[n] = n;
+		else 
 			fibCache[n] = fibDyn(n-1, fibCache) + fibDyn(n-2, fibCache);
-			return fibCache[n];
-		}
+		return fibCache[n];
 	}
 }
